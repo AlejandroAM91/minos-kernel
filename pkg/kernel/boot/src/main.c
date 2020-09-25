7d12889bccdb1dd10be2bc0c615b882f87def83b
Alejandro Alonso Mayo <alejandroalonsomayo@gmail.com>
2020-09-25 07:22:10 +0200

    feat(boot): adds console scroll

--- pkg/kernel/boot/src/main.c
+++ pkg/kernel/boot/src/main.c
@@ -2,5 +2,12 @@
  
 void main(void) 
 {
-	boot_console_puts("MinOS v0.0.0");
+	boot_console_puts("MinOS v0.0.0\n");
+	for (int i = 0; i < 23; i++) {
+		boot_console_puts("No Scroll Line\n");
+	}
+
+	for (int i = 0; i < 10; i++) {
+		boot_console_puts("Scroll Line\n");
+	}
 }
